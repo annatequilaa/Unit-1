ef4297d3b89032dabe0cac4e7f4b6c9d8eaedc64
Ann Tsai <124410631+annatequilaa@users.noreply.github.com>
2024-09-26 19:13:14 +0900

    Add files via upload

---
+++ Project/mylib.py
@@ -0,0 +1,171 @@
+from os import system, name
+
+
+def clear():
+    #windows
+    if name == 'nt':
+        _ = system('cls')
+    #mac
+    else:
+        _ = system('clear')
+
+def n_validation(number:str):
+    for letter in number:
+        if letter not in "0123456789":
+            print("number must be a number, try again.")
+            return False
+        else:
+            return int(number)
+
+def alpha_validation(word):
+    for letter in word:
+        if letter not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            print("input must be alphabets, try again.")
+            return False
+        else:
+            return str(word)
+
+#reuse these for password?
+def get_tasks():
+    task_dict = {}
+    data_cleaned = []
+    with open("todo_db.csv", mode="r") as f:
+        data = f.readlines()
+    for i in data:
+        data_cleaned.append(i.strip())
+    for i in range(len(data_cleaned)):
+        task_dict[i + 1] = data[i]
+    return task_dict
+
+def view_tasks(task_dict):
+    output = ""
+    if len(task_dict) < 1:
+        return "Your to-do list is empty."
+    for n, task in task_dict.items():
+        output += f"{n}. {task}"
+    return output
+
+def add_task(item):
+    data_cleaned = []
+    with open("todo_db.csv", mode="r") as f:
+        data = f.readlines()
+    for i in data:
+        data_cleaned.append(i.strip())
+    if item in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" or item not in data_cleaned:
+        with open("todo_db.csv", mode="a") as f:
+            f.write(f"{item}\n")
+        return True
+    else:
+        return False
+
+
+def delete_task(n):
+    task_dict = get_tasks()
+    if n in task_dict:
+        del task_dict[n]
+        with open("todo_db.csv", mode="w") as f:
+            for task in task_dict.values():
+                f.write(f"{task}")
+        return True
+    else:
+        return False
+
+
+def mark_complete(n):
+    task_dict = get_tasks()
+    if n in task_dict and "(completed)" not in task_dict[n]:
+        task_dict[n] = "(completed) " + task_dict[n]
+        with open("todo_db.csv", "w") as f:
+            for task in task_dict.values():
+                f.write(task)
+        return True
+    else:
+        return False
+
+
+def undo_mark_complete(n):
+    task_dict = get_tasks()
+    if n in task_dict and "(completed)" in task_dict[n]:
+        task_dict[n] = task_dict[n].replace("(completed) ", "")
+        with open("todo_db.csv", "w") as f:
+            f.writelines(task_dict.values())
+        return True
+    else:
+        return False
+
+def get_login():
+    with open('login_db.csv', mode='r') as f:
+        data = f.readlines()
+    dict = {}
+    for item in data:
+        data_cleaned = item.strip()
+        data_separated = data_cleaned.split(', ')
+        username = data_separated[0]
+        password = data_separated[1]
+        dict[username] = password
+    return dict
+
+def view_logins(dict):
+    output = ""
+    if len(dict) < 1:
+        return "There are currently no log in info stored."
+    for username, password in dict.items():
+        output += f"{username}: {undo_cypher(password)}\n"
+    return output
+
+def add_login(username, password):
+    data_cleaned = []
+    with open("login_db.csv", mode="r") as f:
+        data = f.readlines()
+    for i in data:
+        data_cleaned.append(i.strip())
+    if username in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" or username not in data_cleaned:
+        with open("login_db.csv", mode="a") as f:
+            f.write(f"{username}, {cypher(password)}\n")
+        return True
+    else:
+        return False
+
+
+def delete_login(username):
+    dict = get_login()
+    if username in dict:
+        del dict[username]
+        with open('login_db.csv', 'w') as f:
+            for username, password in dict.items():
+                f.write(f"{username}, {cypher(password)}\n")
+        return True
+    else:
+        return False
+
+def update_login(username,n_password):
+    dict = get_login()
+    if username in dict:
+        dict[username] = n_password
+        with open('login_db.csv', 'w') as f:
+            for username, password in dict.items():
+                f.write(f"{username}, {cypher(password)}\n")
+        return True
+    else:
+        return False
+
+
+def cypher(password):
+    output = ""
+    string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()<_+"
+    for char in password:
+        if char in string:
+            i = string.index(char)
+            new_index = (i + 12) % len(string)
+            output += string[new_index]
+    return output
+
+def undo_cypher(c_password):
+    output = ""
+    string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()<_+"
+    for char in c_password:
+        if char in string:
+            i = string.index(char)
+            new_index = (i - 12) % len(string)
+            output += string[new_index]
+    return output
---
+++ Project/todo.py
@@ -0,0 +1,168 @@
+
+#import functions
+from mylib import *
+from time import sleep
+
+#colors
+end_code = "\033[00m"
+red = "\33[0;31m"
+yellow = "\33[0;33m"
+green = "\033[0;32m"
+blue = "\33[0;34m"
+purple = "\33[0;35m"
+cyan = "\33[0;36m"
+
+#dictionary
+catalog = {}
+
+
+#start screen
+def todo_app():
+    print(f"{green}Welcome to your personal to-do list, what would you like to do?\n")
+    while True:
+        print(f"{red}add task (A)    {yellow}view task list (V)    {green}delete task (D)\n{cyan}mark as complete (M)    {blue}undo mark as complete (U)    {purple}something (S)    {end_code}quit (Q)\n")
+        u_option = input(f"{green}Enter the letter in the brackets to select: {end_code}")
+        option = alpha_validation(u_option)
+        if option == False:
+            print(f"{end_code}_____________________________\n")
+            continue
+
+        if option == "a" or option == "A":
+            task = input(f"{red}Enter the task you want to add: ")
+            validness = add_task(task)
+            if not validness == False:
+                print(f"{yellow}\nHere's your updated to-do list: \n{view_tasks(get_tasks())}")
+            else:
+                print(f"{red}task cannot be added because it either already exists or the format is wrong")
+                print(f"{end_code}_____________________________\n")
+                continue
+
+        elif option == "v" or option == "V":
+            print(f"{yellow}\nHere's your current to-do list: \n{view_tasks(get_tasks())}")
+
+        elif option == "d" or option == "D":
+            print(f"{yellow}\nHere's your current to-do list: \n{view_tasks(get_tasks())}")
+            delete = n_validation(input(f"{green}Enter the no. of the task you want to delete: "))
+            validness = delete_task(delete)
+            if not validness == False:
+                print(f"{yellow}\nHere's your updated to-do list: \n{view_tasks(get_tasks())}")
+            else:
+                print(f"{end_code}Entered task does not exist")
+
+        elif option == "m" or option == "M":
+            print(f"{yellow}\nHere's your current to-do list: \n{view_tasks(get_tasks())}")
+            n = n_validation(input(f"{green}Enter the no. of the task you want to mark as complete: "))
+            validness = mark_complete(n)
+            if not validness == False:
+                print(f"{yellow}\nHere's your updated to-do list: \n{view_tasks(get_tasks())}")
+            else:
+                print(f"{end_code}Entered task does not exist or is already completed")
+
+        elif option == "u" or option == "U":
+            print(f"{yellow}\nHere's your current to-do list: \n{view_tasks(get_tasks())}")
+            n = n_validation(input(f"{green}Enter the no. of the task you want to undo mark as complete: "))
+            validness = undo_mark_complete(n)
+            if not validness == False:
+                print(f"{yellow}\nHere's your updated to-do list: \n{view_tasks(get_tasks())}")
+            else:
+                print(f"{end_code}Entered task either does not exist or is not marked complete")
+
+        elif option == "s" or option == "S":
+            code_input = input(f"{purple}Enter a code: ")
+            if code_input == "vand3r":
+                break
+            else:
+                print(f"\n{end_code}Invalid, try again or {red} GET LOST. {green}\n")
+                continue
+
+        elif option == "q" or option == "Q":
+            return False
+            break
+
+        else:
+            print(f"{end_code}Input invalid, try again")
+        print(f"{end_code}_____________________________\n")
+
+
+def password_manager():
+    clear()
+    print(f"{end_code}processing...")
+    for i in range(10):
+        print("...")
+        sleep(0.5)
+    print(f"\nWelcome to the password manager.")
+    while True:
+        # Create, Replace, Update, Delete
+        print(f"{red}create login(C)    {yellow}view logins (V)    {green}update password (U)    {blue}delete login (D)    {purple}quit (Q){end_code}")
+        u_pswd_option = input("Enter the letter in the brackets to select what you want to do: ")
+        p_option = alpha_validation(u_pswd_option)
+        if p_option == "Q" or p_option == "q":
+            clear()
+            print(f"{end_code}_____________________________\n")
+            break
+        elif p_option not in "CVUDQcvudq":
+            print(f"{end_code}Input invalid...")
+            print(f"{end_code}_____________________________\n")
+            continue
+        code_input = input(f"{end_code}Enter the code again to continue: ")
+        if code_input != "vand3r":
+            print(f"\n{end_code}Invalid, try again. {blue} (How did you even get in here?) {green}\n")
+        else:
+            clear()
+            if p_option == False:
+                print(f"{end_code}_____________________________\n")
+                continue
+
+            elif p_option == "c" or p_option == "C":
+                username = input(f"{red}Enter the username you want to store: ")
+                if username == "":
+                    print(f"{end_code}Username cannot be blank!")
+                    continue
+                password = input(f"{red}Enter the password you want to store: ")
+                if username == "":
+                    print(f"{end_code}Password cannot be blank!")
+                    continue
+                validness = add_login(username, password)
+                if not validness == False:
+                    print(f"{yellow}\nHere's your updated list of log-in infos: \n{view_logins(get_login())}")
+
+            elif p_option == "v" or p_option == "V":
+                print(f"\n{yellow}Here's the current list of log-in infos: \n{view_logins(get_login())}")
+
+            elif p_option == "d" or p_option == "D":
+                print(f"{yellow}\nHere's your current list of log-in infos: \n{view_logins(get_login())}")
+                if len(get_login()) < 1:
+                    continue
+                delete = input(f"{blue}Enter the username of the log-in info you want to delete: ")
+                validness = delete_login(delete)
+                if not validness == False:
+                    print(f"{yellow}\nHere's your updated list of log-in infos: \n{view_logins(get_login())}")
+                else:
+                    print(f"{end_code}Username entered is blank or doesn't exist")
+
+            elif p_option == "u" or p_option == "U":
+                print(f"{yellow}\nHere's your current list of log-in infos: \n{view_logins(get_login())}")
+                if len(get_login()) < 1:
+                    continue
+                username = input(f"{green}Enter the username of the password you want to update: ")
+                n_password = input("Enter the new password: ")
+                validness = update_login(username,n_password)
+                if not validness == False:
+                    print(f"{yellow}\nHere's your updated list of log-in infos: \n{view_logins(get_login())}")
+                else:
+                    print(f"{end_code}Username entered is blank or doesn't exist")
+
+
+
+            print(f"{end_code}_____________________________\n")
+
+while True:
+    check = todo_app()
+    if check == False:
+        print(f"{red}GOODBYE.")
+        exit(1)
+    password_manager()
+
+
+
+
